862ad5dd8a72272124c684ea3cdc3657c75b9d97
Rudy Ravelin <rudy@noukod.com>
2018-09-03 14:19:15 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -16,6 +16,6 @@ def position_taken?(board, position)
   if board[position] == " " || board[position] == "" || board[position] == nil
     return false
   else
-  return false
+  return true
 end
 end
