8a1cf716d866ccca537d4a80fa25eca502ef64ca
Rudy Ravelin <rudy@noukod.com>
2018-09-03 15:18:23 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -10,7 +10,7 @@ end
 
 # position.to_i.between?(0,8) && !position_taken?(board, position.to_i-1)
 def on_board?(num)
-  if num.between(0,8) == true
+  if num.between?(0,8) == true
     return true
   else
     return false
